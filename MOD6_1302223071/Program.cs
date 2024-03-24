bfbc2304770931fed87694df3b03b78dcc301652
Aldinorasend <aldinorasend@gmail.com>
2024-03-25 01:38:14 +0700

    first

---
+++ MOD6_1302223071/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD6_1302223071
+{
+    class Program
+    {
+    
+            static void Main(string[] args)
+            {
+               
+
+            }
+    }
+}
+ 
+
+    
+
