7093768f96f4b1a935864564cce71d873f693958
Aldinorasend <aldinorasend@gmail.com>
2024-03-25 01:41:40 +0700

    menambahkan class SayaTubeVideo

--- MOD6_1302223071/Program.cs
+++ MOD6_1302223071/Program.cs
@@ -9,12 +9,43 @@ namespace MOD6_1302223071
 {
     class Program
     {
-    
+        public class SayaTubeVideo
+        {
+            private int id;
+            private string title;
+            private int playCount;
+
+            public SayaTubeVideo(String title)
+            {
+                this.title = title;
+                Random rand = new Random();
+                int min = 100000;
+                int max = 999999;
+                this.id = rand.Next(min, max + 1);
+                this.playCount = 0;
+            }
+
+            public void IncreasePlayCount(int playCount)
+            {
+                this.playCount = this.playCount + playCount;
+            }
+
+            public void PrintVideoDetails()
+            {
+                Console.WriteLine("ID Video : " + id);
+                Console.WriteLine("Title Video : " + title);
+                Console.WriteLine("Jumlah Tontonan : " + playCount);
+            }
+
             static void Main(string[] args)
             {
-               
+                String vid1 = "Tutorial Design By Contract - Aldinorasend";
+                SayaTubeVideo sayaTubeVideo = new SayaTubeVideo(vid1);
+                sayaTubeVideo.PrintVideoDetails();
+                Console.Read();
 
             }
+        }
     }
 }
  
